b5968a157cb78582894d3e0b74b30383bcd84659
Truongxt <truongxt2501@gmail.com>
2026-03-30 09:00:29 +0700

    update require

--- server/index.js
+++ server/index.js
@@ -8,9 +8,11 @@ const socketConfig = require("./config/socket");
 
 
 const userRoutes = require("./routes/userRoutes");
+
+
 const friendRoutes = require("./routes/friendRoutes");
 const app = express();
-const groupRoutes = require("./routes/groupRoutes");
+
 // ===== Middleware =====
 app.use(cors());
 app.use(express.json());
@@ -23,8 +25,8 @@ const messageRoutes = require("./routes/messageRoutes");
 app.use("/api/users", userRoutes);
 app.use("/api/conversations", conversationRoutes);
 app.use("/api/messages", messageRoutes);
+
 app.use("/api/friends", friendRoutes);
-app.use("/api/groups", groupRoutes); 
 // ===== Health check =====
 app.get("/", (req, res) => {
   res.send("API is running...");
